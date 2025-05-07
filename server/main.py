e3f72a6c538377ab658ebcd4e75b1b9003d36bf3
muntaqam <maahi305@yahoo.com>
2025-05-07 17:46:16 -0400

    fix: update main.py to complete Sprint 1 setup

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi.security import OAuth2PasswordBearer
 from server.db import init_db
 from server.routes.auth import router as auth_router 
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +13,13 @@ async def lifespan(app: FastAPI):
     print("server shutdown..")
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # <-- React dev server port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 
 @app.get("/")
